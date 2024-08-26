473de84e0161be6319d8a1ccb7608a27502e0a72
immaryam <122366273+immaryam@users.noreply.github.com>
2024-08-26 20:14:04 +0300

    Create style.css

---
+++ module2-solution/style.css
@@ -0,0 +1,74 @@
+section{
+position:relative;
+}
+p
+{
+margin-top: 10%;
+}
+.top-right{
+position:absolute;
+margin:0;
+top:0;
+right:0;
+padding:5px;
+}
+
+#section1 h3{
+background-color:pink;
+border: 1px solid black;
+margin-bottom: 5px;
+}
+#section2 h3{
+background-color:darkred;
+border: 1px solid black;
+}
+
+#section3 h3 {
+background-color:yellow;
+border: 1px solid black;
+}
+@media(min-width:992px){
+.col-lg-4{
+float:left;
+margin: 10px;
+padding: 5px;
+border:1px solid black;
+}
+
+.col-lg-4{
+width: 33%;
+}
+}
+@media(min-width:768px) and(max-width:991px){
+.col-md-6,.col-md-12{
+float:left;
+margin: 10px;
+padding: 5px;
+border:1px solid black;
+}
+.col-md-6{
+width: 50%;
+}
+.col-md-12{
+width: 100%;
+}
+}
+
+@media(max-width:767px){
+.col-sm-12,{
+float:left;
+margin: 10px;
+padding: 5px;
+border:1px solid black;
+}
+.col-sm-12{
+width: 100%;
+}
+}
+
+h1 {
+text-align:center;
+}
+
+
+</style>
